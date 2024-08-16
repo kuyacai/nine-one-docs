5fe9bdf02b6ecd7f83f98d9685b070987e814be4
kuyacai <24796839@qq.com>
2024-08-17 02:08:02 +0800

    Initial commit 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import PlurkLayout from '@site/src/components/plurk/PlurkLayout';
 import Hero from '@site/src/components/plurk/Hero';
+import HeroOneApiWithSwiper from '@site/src/components/plurk/HeroOneApiWithSwiper';
+
 
 export default function MyPage() {
   return (
     
       <PlurkLayout title="My Custom Page">
         <Hero />
+        <HeroOneApiWithSwiper />
       </PlurkLayout>
    
   );
