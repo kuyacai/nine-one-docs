c7e0cc660d7471c2771fa17412386854f4ff8861
kuyacai <24796839@qq.com>
2024-08-24 19:55:50 +0800

    Update baseUrl for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: 'https://nineone.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/nine-one-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
