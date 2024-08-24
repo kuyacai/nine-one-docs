6808eb9f2b4940f401b7909a1a875ac9a0ba70dd
kuyacai <24796839@qq.com>
2024-08-24 13:34:44 +0800

    Init commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,19 +12,19 @@ dotenv.config();
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Nine One AI',
+  tagline: 'Nine One AI are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nineone.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'kuyacai', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
